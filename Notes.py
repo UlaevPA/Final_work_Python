4cca1eee1b3cd235c25722ab230311c8c9fa49ae
ulaev <ulaev1990@mail.ru>
2024-02-26 10:54:19 +0300

    Create code

--- Notes.py
+++ Notes.py
@@ -1,68 +1,81 @@
+import csv
+import datetime
+import pandas as pd
 
-def show_all(file_name:str):
-   with open(file_name, 'r', encoding='utf-8') as f:
-        data = f.readlines()
-        for i, v in enumerate(data, 1):
-            print(i,'.', v, '\n')
-           
+def show_all(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        for row in reader:
+            print("ID заметки", "Название заметки",
+                  "Текст заметки", "Дата заметки")
+            print(row["ID заметки"], row["Название заметки"],
+                  row["Текст заметки"], row["Дата заметки"])
 
 
-def remove(file_name:str):
-    lines=[]
-    with open(file_name, 'r', encoding='utf-8') as old:
-        lines = old.readlines()
-    with open(file_name, 'w', encoding='utf-8') as new:
-        line_del = int(input("Введите номер строки для удаления: "))
-        for number, line in enumerate(lines, 1):
-            if number != line_del:
-                new.write(line)
-        
+def remove(file_name: str):
+    df = pd.read_csv(file_name)
+    print(df)
+    del_line = int(input('Введите индекс строки для удаления: '))
+    df = df.drop(del_line)
+    df.to_csv(file_name, index=False)
+            
 
-def modify(file_name:str):
-   
- 
-    old_data = find_by_name_note(file_name, True)
     
-    print("Введите новые данные:\n")
-    new_note = input('Название заметки: ')
-    body_note = input('Текст заметки: ')
-    
-    data = ""
-    with open(file_name, 'r',encoding='utf-8') as f:
-        data = f.readlines()
-        i = data.index(old_data)
-        data[i] = f'{new_note}: {body_note}\n'
-        
-    with open(file_name, 'w',encoding='utf-8') as f:\
-        f.writelines(data)
 
-        
-        
+
+def modify(file_name: str):
+    df = pd.read_csv(file_name)
+    print(df)
+
+    mod_line = input('Введите ID заметки: ')
+    fields_to_change = input('Какие данные Вы хотите изменить? ').split(',')
+    data_to_update = {}
+    for field in fields_to_change:
+        data_to_update[field] = input(f'Введите {field}: ')
+
+    df = pd.read_csv(file_name).set_index('ID заметки')
+    df.loc[mod_line, fields_to_change] = data_to_update
+    df.to_csv(file_name)
+    print('Спасибо, данные обновлены')
+    
+
+
 def find_by_name_note(file_name: str, option: bool):
-    
-    with open(file_name, 'r',encoding='utf-8') as f:
+
+    with open(file_name, 'r', encoding='utf-8') as f:
         line = f.readlines()
-        str_num = int(input("Введите строку заметки: "))
+        str_num = int(input("Введите строку заметки, кроме первой: "))
         for i, v in enumerate(line, 1):
-            if i == str_num:
-              return ("".join(v))
-        
-       
+            if i == 1:
+                print("Первая строка является шапкой заметки")
+            elif i == str_num:
+                return ("".join(v))
+
+
+def fields(file_name: str):
+    field = ["ID заметки", "Название заметки", "Текст заметки", "Дата заметки"]
+    with open(file_name, '+r', encoding='utf-8', newline='') as f:
+        data = csv.writer(f, delimiter=';', lineterminator='\n')
+        data.writerow(field)
+
 
 def add_new(file_name: str):
- 
+    from csv import writer
+    id_num = int(input('Введите ID заметки: '))
     new_note = input('Новая заметка: ')
     body_note = input('Текст заметки: ')
+    date_create = datetime.date.today()
+    all_name = [id_num, new_note.title(), body_note.title(), date_create]
+    with open(file_name, 'a+', encoding='utf-8', newline='') as f:
+        data = writer(f, delimiter=';', lineterminator="\r")
+        data.writerow(all_name)
+        fields(file_name)
 
-    with open(file_name, 'a', encoding='utf-8') as fd:
-        fd.write(f'{new_note}: {body_note} \n')
-        
-        
 
- 
 def main():
-    file_name = 'notes.txt'
+    file_name = 'notes.csv'
     flag_exit = False
+
     while not flag_exit:
         print('1 - показать все заметки')
         print('2 - добавить заметку')
@@ -79,11 +92,12 @@ def main():
         elif answer == '4':
             modify(file_name=file_name)
         elif answer == '5':
-            print(find_by_name_note(file_name,False))
+            print(find_by_name_note(file_name, False))
         elif answer == 'q':
             flag_exit = True
         else:
             print('Введена неверная команда!')
+
 
 if __name__ == '__main__':
     main()
